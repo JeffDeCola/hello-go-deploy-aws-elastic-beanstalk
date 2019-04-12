d54cc1ff84b0ab4c67b629c5cc397620bffdd54b
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2019-04-12 10:45:30 -0700

    update docker build in concourse

--- ci/scripts/build-push.sh
+++ ci/scripts/build-push.sh
@@ -32,48 +32,55 @@ echo "List whats in the current directory"
 ls -la
 echo " "
 
-echo "Setup the GOPATH based on current directory"
-export GOPATH=$PWD
+echo "Copy entire repo into dist"
+cp -r hello-go-deploy-aws-elastic-beanstalk/* dist
 echo " "
 
-echo "Now we must move our code from the current directory ./hello-go-deploy-aws-elastic-beanstalk to" 
-echo "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk"
-mkdir -p src/github.com/JeffDeCola/
-cp -R ./hello-go-deploy-aws-elastic-beanstalk src/github.com/JeffDeCola/.
-echo " "
+echo "Copy the Dockefile in dist/Dockerfile"
+cp dist/example-01/build-push/Dockerfile dist
 
-echo "cd src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk/example-01"
-cd src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk/example-01
-echo " "
+#echo "Setup the GOPATH based on current directory"
+#export GOPATH=$PWD
+#echo " "
 
-echo "Check that you are set and everything is in the right place for go:"
-echo "gopath is: $GOPATH"
-echo "pwd is: $PWD"
-ls -la
+#echo "Now we must move our code from the current directory ./hello-go-deploy-aws-elastic-beanstalk to" 
+#echo "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk"
+#mkdir -p src/github.com/JeffDeCola/
+#cp -R ./hello-go-deploy-aws-elastic-beanstalk src/github.com/JeffDeCola/.
+#echo " "
 
-echo "Create a binary hello-go in /bin"
-go build -o bin/hello-go main.go
-echo ""
+#echo "cd src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk/example-01"
+#cd src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk/example-01
+#echo " "
 
-echo "cd to the /dist directory"
-cd "$GOPATH/dist"
-echo " "
+#echo "Check that you are set and everything is in the right place for go:"
+#echo "gopath is: $GOPATH"
+#echo "pwd is: $PWD"
+#ls -la
 
-echo "cp the binary into /dist"
-cp "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk/example-01/bin/hello-go" .
-echo " "
+#echo "Create a binary hello-go in /bin"
+#go build -o bin/hello-go main.go
+#echo ""
 
-echo "cp the Dockerfile into /dist"
-cp "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk/example-01/build-push/Dockerfile" .
-echo " "
+#echo "cd to the /dist directory"
+#cd "$GOPATH/dist"
+#echo " "
 
-echo "Make it executable by all - chmod +x"
-chmod +x hello-go
-echo " "
+#echo "cp the binary into /dist"
+#cp "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk/example-01/bin/hello-go" .
+#echo " "
 
-echo "List whats in the /dist directory"
-ls -la
-echo " "
+#echo "cp the Dockerfile into /dist"
+#cp "$GOPATH/src/github.com/JeffDeCola/hello-go-deploy-aws-elastic-beanstalk/example-01/build-push/Dockerfile" .
+#echo " "
+
+#echo "Make it executable by all - chmod +x"
+#chmod +x hello-go
+#echo " "
+
+#echo "List whats in the /dist directory"
+#ls -la
+#echo " "
 
 echo "The concourse pipeline will build and push the docker image to DockerHub"
 echo " "
